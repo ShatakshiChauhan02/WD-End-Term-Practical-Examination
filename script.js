2584ec351dd139fa8c846f29d6cc543b28df01db
ShatakshiChauhan02 <shatakshichauhanabes28@gmail.com>
2026-06-18 13:36:00 +0530

    Vowel and consonant Checker

---
+++ script.js
@@ -0,0 +1,18 @@
+function checkCharacter() {
+    let ch = document.getElementById("charInput").value.toLowerCase();
+    let result = document.getElementById("result");
+
+    if (ch === "") {
+        result.innerHTML = "Please enter a character.";
+    }
+    else if (ch === 'a' ||ch === 'e' ||ch === 'i' ||ch === 'o' ||ch === 'u') 
+        {
+        result.innerHTML = ch.toUpperCase() + " is a Vowel.";
+    }
+    else if (ch >= 'a' && ch <= 'z') {
+        result.innerHTML = ch.toUpperCase() + " is a Consonant.";
+    }
+    else {
+        result.innerHTML = "Please enter a valid alphabet.";
+    }
+}
